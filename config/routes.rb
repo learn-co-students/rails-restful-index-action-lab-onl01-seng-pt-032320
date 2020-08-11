14f7ca428511e6ca7eb8022676c481ebd0b2e89b
github username <github email address>
2020-08-11 17:22:04 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#routes.rb = where our routes are defined
+
+# get '/patients/:id', to: 'patients#show'
+# get '/students', to: 'students#index'
+resources :students, only: [:index, :new]
 end
